2dbff1110a8f3e51eca80896956e811312e26e67
Serhii Khobotov <hobbitura@gmail.com>
2024-07-31 21:25:52 +0300

    make build

---
+++ index.c7434bac.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".field-text").forEach(function(e){var t="".concat(e.getAttribute("name")).replace("N"," N"),a=t.replace(t[0],t[0].toUpperCase()),c=document.createElement("label");c.classList.add("field-label"),c.setAttribute("for","".concat(e.getAttribute("id"))),c.textContent="".concat(t.toUpperCase()),e.setAttribute("placeholder",a),e.before(c)});
+//# sourceMappingURL=index.c7434bac.js.map
